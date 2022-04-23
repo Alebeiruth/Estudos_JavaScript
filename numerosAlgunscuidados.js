426f5363551f7a8336cbaf5f2a324e0e69ac636b
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2022-04-22 23:33:53 -0300

    Update numerosAlgunscuidados.js

--- numerosAlgunscuidados.js
+++ numerosAlgunscuidados.js
@@ -1,5 +1,5 @@
 console.log(7 / 0); // java possui um indicador quando algum resultado é infinito INFINITY
-console.log("10"/ 2); //mesmo sendo string java reconhece mcomo numero
+console.log("10"/ 2); //mesmo sendo string java reconhece como numero
 console.log("33"+ 2); // contatena (uni) em vez de somar, string ganha aqui
 console.log("33"- 2);  // aqui não ganha por causa do negativo
 console.log("Show"* 2);
